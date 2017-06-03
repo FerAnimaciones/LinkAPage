2920c8360197997532684298c103c09d9a8dcf61
Fernando Manuel Avila Cataño <feranimaciones@gmail.com>
2017-06-02 22:12:27 -0600

    Act 02/06/2017

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # LinkAPage
 LinkAPage Proyecto Escolar HTML PHP
+
+# Funcionalidades
+
+* Regitro
+* Modificacion
+* Eliminacion
+
+
+# Screenshots
+![Alt text](/imagenes/capturas/cap1.png)
+![Alt text](/imagenes/capturas/cap2.png)
+![Alt text](/imagenes/capturas/cap3.png)
+![Alt text](/imagenes/capturas/cap4.png)
+![Alt text](/imagenes/capturas/cap5.png)
+
+## Community
+* Pagina oficial de feranimaciones [Official Website](feranimaciones.esy.es/).
--- paginas/include/encabezado.php
+++ paginas/include/encabezado.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html>
-<head> 
+<head>
 	<title>LinkApages</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta name="keywords" content="programacion,videos,imagenes,Programacion Web,Web Programacion">
@@ -10,21 +10,21 @@
 	<link href="../css/gridview.css" rel="stylesheet">
 	<script src="../scripts/botones.js"></script>
 	<link rel="icon" href="../imagenes/favicon.ico">
-</head>  
-<body onload="inicializar()">  
+</head>
+<body onload="inicializar()">
 	<div id="div_principal">
-		<header> 
+		<header>
 			<?php session_start(); ?>
 			<div class="renglon">
-				<div class="col-6"> 
+				<div class="col-6">
 					<h1> LinkAPage</h1>
 				</div>
-				<div class="col-6"> 
-					<?php 
+				<div class="col-6">
+					<?php
 					if(isset($_SESSION["nombreusuario"]))
 					{
 						echo "
-						
+
 						<a class='ingresar'   href='salir.php' >Te has identificado como:".$_SESSION["nombreusuario"]."(Salir)</a>
 						";
 					}
@@ -37,43 +37,39 @@
 					?>
 				</div>
 			</div>
-		</header>   
-		<div class="renglon"> 
-			<nav class="col-12"> 
+		</header>
+		<div class="renglon">
+			<nav class="col-12">
 				<ul>
 					<li class="menu-item"> <a href="index.php"> PRINCIPAL </a>
-					</li>	
-					<li  class="menu-item"> <a href="#"> CUENTA </a> 
+					</li>
+					<li  class="menu-item"> <a href="#"> CUENTA </a>
 						<ul class="submenu">
 
 
-							<?php 
+							<?php
 							if(isset($_SESSION["nombreusuario"]))
 							{
-								echo ' <li> <a href="ingresar.php">Preferencias</a></li> 
+								echo ' <li> <a href="ingresar.php">Preferencias</a></li>
 								<li> <a href="salir.php">Salir</a></li> ';
 							}
 							else
 							{
-								
+
 								echo '
 								<li> <a href="registro.php">Registrarse</a></li>
 								<li> <a href="ingresar.php">Ingresar</a></li>';
 							}
 							?>
-
-
-
-							
 						</ul>
-					</li>	
+					</li>
 					<li  class="menu-item"> <a href="#">AYUDA</a>
 						<ul class="submenu">
 							<li> <a href="mapa.php"> Mapa del sitio </a></li>
 							<li> <a href="#"> Contacto </a></li>
-							<li> <a href="acerca.php"> Acerca de... </a></li>		
+							<li> <a href="acerca.php"> Acerca de... </a></li>
 						</ul>
-					</li>	
+					</li>
 					<?php
 					if(isset($_SESSION["nombreusuario"]))
 					{
@@ -82,7 +78,7 @@
 							echo '
 							<li  class="menu-item"> <a href="#">ADMINISTRACION</a>
 								<ul class="submenu">
-									<li> <a href="usuarios.php"> Ver Usuarios </a></li>		
+									<li> <a href="usuarios.php"> Ver Usuarios </a></li>
 								</ul>
 							</li>
 							';
@@ -92,4 +88,4 @@
 				</ul>
 			</nav>
 		</div>
-		<main>	
+		<main>
